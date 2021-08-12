2aed3ecf18131493ad4b046a860bad3f9b935461
Roman <katyrinroman@mail.ru>
2021-08-12 21:53:14 +0700

    Create main screen

--- app/src/main/java/com/katyrin/testproductivityinside/MainActivity.kt
+++ app/src/main/java/com/katyrin/testproductivityinside/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.katyrin.testproductivityinside
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.katyrin.testproductivityinside.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding?.requestButton?.setOnClickListener { }
+        binding?.uploadButton?.setOnClickListener { }
+    }
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
     }
 }
